7b0b13546924d96287fa0f75ccc28508da97a6a4
JeongYoonhwan <legogo92@naver.com>
2023-09-06 13:35:19 +0900

    Initial Commit

---
+++ VideoCollectionViewTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoCollectionViewTransition
+//
+//  Created by Schemalism on 2023/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
